21037f6e501bd2515d15ff181630933e2ac7a9b4
JacksonLvweijiang <1550335887@qq.com>
2025-02-19 14:00:44 +0800

    second commit

--- test.py
+++ test.py
@@ -5,4 +5,11 @@ from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
 
 
+transform=transforms.Compose([transforms.ToTensor(),
+                              transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+train_dataset = datasets.MNIST(root='./data', train=True, transform=transform, download=True)
+test_dataset = datasets.MNIST(root='./data', train=False, transform=transform, download=True)
+train_loader = DataLoader(dataset=train_dataset, batch_size=64, shuffle=True)
+test_loader = DataLoader(dataset=test_dataset, batch_size=64, shuffle=True)
 
